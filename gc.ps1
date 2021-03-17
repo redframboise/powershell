5e71f184d6e752424a12012fd1eeca7c846ae3ed
im just a flower <53564088+redframboise@users.noreply.github.com>
2021-03-17 14:35:18 +0100

    Update gc.ps1

--- gc.ps1
+++ gc.ps1
@@ -28,13 +28,14 @@
                                     Remove-Item $target -Recurse -Force -ErrorAction silentlycontinue
                                     Write-Host "`r`n                         > [$server] Target has been deleted." -foreground red
                                     
-                                    <#  #>
+                                    <# Get target info, left size #>
                                     $AreYouStillThere = Test-Path -Path $target
                                     if (!($AreYouStillThere)) {
                                             Write-Host "`r`n                         > [$server] Success !"  -foreground green
                                     
                                     } else { 
                                     
+                                    $target = "\\$server\$folder"
                                     $size = Get-All-Size $target -ErrorAction silentlycontinue
                                     Write-Host "`r`n                         > [$server] $target is not empty, size is about $size."  -foreground yellow
                                     Write-Host "Keep in mind that it could be a false negative result because a process might be running.`r`n"
