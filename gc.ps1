22c80d042aed68d6be1f680d6d735fa89a509511
im just a flower <53564088+redframboise@users.noreply.github.com>
2021-03-17 14:00:53 +0100

    Update gc.ps1

--- gc.ps1
+++ gc.ps1
@@ -1,7 +1,43 @@
-<##>
-$servers = @('192.168.0.7','OrAnotherServer','etc')
+<# There will be a little note to adapt the code #>
 
-$folder = "\\$server\C$\Somewhere"
-$everythinginthatfolder = "\\$server\C$\Somewhere\*"
+    <# DoNotModify #> 
+                    function Get-All-Size
+                        {
+                        param([string]$pth)
+                        "{0:n2}" -f ((gci -path $pth -recurse | measure-object -property length -sum).sum /1mb) + " Mb"
+                        }
 
+    <# Modify #>    $servers = @('192.168.0.7','OrAnotherServer','etc')
     
+    <# Comment #> # $target = "\\$server\$folder"   
+    
+    <# Modify #>    $folder = "C$\folder"
+
+    <# DoNotModify #>       foreach ($server in $servers)
+                                {
+    
+                                    $target = "\\$server\$folder"
+
+                                    #
+                                    $size = Get-All-Size $target -ErrorAction silentlycontinue
+                                    Write-Host "`r`n                         > [$server] Target size is about $size." -foreground yellow
+                                    Write-Host
+                                    
+                                    #
+                                    $target = "\\$server\$folder\*"
+                                    Remove-Item $target -Recurse -Force -ErrorAction silentlycontinue
+                                    Write-Host "`r`n                         > [$server] Target has been deleted." -foreground red
+                                    
+                                    # 
+                                    $AreYouStillThere = Test-Path -Path $target
+                                    if (!($AreYouStillThere)) {
+                                            Write-Host "`r`n                         > [$server] Success !"  -foreground green
+                                    
+                                    } else { 
+                                    
+                                    Write-Host "`r`n                         > [$server] $target is not empty !"  -foreground yellow
+                                    Write-Host "Keep in mind that it could be a false negative result, because a process might be using a file.`r`n"
+                                    
+                                    }
+
+                                } Write-Host
